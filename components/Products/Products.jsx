8d8f6095e73cc0f722cba9fdc7c362457f5c8730
Erbol <a1darbekov640@gmail.com>
2022-07-25 17:47:32 +0300

    Add Products

--- components/Products/Products.jsx
+++ components/Products/Products.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Products.module.sass";
+import axios from "../../api/axios";
 
 function Products() {
+  const [doors, setDoors] = useState([]);
+  useEffect(() => {
+    const getDoors = async () => {
+      try {
+        const res = await axios.get(`/doors?populate=image`);
+        if (!res.data) {
+          throw new Error();
+        }
+        setDoors(res.data.data);
+      } catch (error) {
+        setDoors(false);
+      }
+    };
+    getDoors();
+  }, []);
+  console.log(doors);
   return (
-    <div className={styles.container}>
+    <div className="container">
       <div className={styles.products}>
         <div className={styles.products__titles}>
           <h3 className={styles.products__title}>Наши товары</h3>
@@ -12,33 +29,33 @@ function Products() {
             Казахстана и Белоруссии
           </p>
         </div>
-        <div className={styles.doors}>
-          <div className={styles.door}>
-            <div className={styles.door__image}>
-              <img
-                className={styles.door__img}
-                src="images/doors.example-1.png"
-                alt="doors"
-              />
-            </div>
-          </div>
-          <div className={styles.door}>
-            <div className={styles.door__image}>
-              <img
-                className={styles.door__img}
-                src="images/doors.example-1.png"
-                alt="doors"
-              />
-            </div>
-          </div>
-          <div className={styles.door}>
-            <div className={styles.door__image}>
-              <img
-                className={styles.door__img}
-                src="images/doors.example-1.png"
-                alt="doors"
-              />
-            </div>
+        <div className={styles.flex}>
+          <button className={styles.doors__title}>Входные</button>
+          <div className={styles.doors}>
+            {doors.map((door) => {
+              return (
+                <div key={door.id} className={styles.door}>
+                  <div className={styles.door__image}>
+                    <img
+                      className={styles.door__img}
+                      src={door.image.url}
+                      alt="doors"
+                    />
+                  </div>
+                  <div className={styles.doorInfo}>
+                    <div className={styles.logo}>
+                      <img src="images/PLANETA_DOORS.png" alt="logo" />
+                    </div>
+                    <div className={styles.door__info}>
+                      <h3 className={styles.doorName}>{door.name}</h3>
+                      <h3 className={styles.width}>Ширина: {door.height}м</h3>
+                      <p className={styles.info}>{door.info}</p>
+                      <h3 className={styles.made}>Сделано: {door.madein}</h3>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
