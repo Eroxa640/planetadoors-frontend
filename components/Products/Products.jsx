3f0c200cae3e377e3fecea8d75ec17df1e29cdd3
Erbol <a1darbekov640@gmail.com>
2022-07-26 13:53:49 +0300

    fix product

--- components/Products/Products.jsx
+++ components/Products/Products.jsx
@@ -32,13 +32,40 @@ function Products() {
         <div className={styles.flex}>
           <h3 className={styles.doors__title}>Входные</h3>
           <div className={styles.doors}>
-            {doors?.map((door) => {
-              return (
-                <div key={door.id} className={styles.door}>
+            {doors.length === 3 ? (
+              doors.map((door) => {
+                return (
+                  <div key={door.id} className={styles.door}>
+                    <div className={styles.door__image}>
+                      <img
+                        className={styles.door__img}
+                        src={door.image.url}
+                        alt="doors"
+                      />
+                    </div>
+                    <div className={styles.doorInfo}>
+                      <div className={styles.logo}>
+                        <img src="images/PLANETA_DOORS.png" alt="logo" />
+                      </div>
+                      <div className={styles.door__info}>
+                        <h3 className={styles.doorName}>{door.name}</h3>
+                        <h3 className={styles.height}>
+                          Высота: {door.height}м
+                        </h3>
+                        <p className={styles.info}>{door.info}</p>
+                        <h3 className={styles.made}>Сделано: {door.madein}</h3>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })
+            ) : (
+              <>
+                <div className={styles.door}>
                   <div className={styles.door__image}>
                     <img
                       className={styles.door__img}
-                      src={door.image.url}
+                      src="images/door-icon.png"
                       alt="doors"
                     />
                   </div>
@@ -47,15 +74,55 @@ function Products() {
                       <img src="images/PLANETA_DOORS.png" alt="logo" />
                     </div>
                     <div className={styles.door__info}>
-                      <h3 className={styles.doorName}>{door.name}</h3>
-                      <h3 className={styles.height}>Высота: {door.height}м</h3>
-                      <p className={styles.info}>{door.info}</p>
-                      <h3 className={styles.made}>Сделано: {door.madein}</h3>
+                      <h3 className={styles.doorName}>?</h3>
+                      <h3 className={styles.height}>Высота: </h3>
+                      <p className={styles.info}>?</p>
+                      <h3 className={styles.made}>Сделано: ?</h3>
                     </div>
                   </div>
                 </div>
-              );
-            })}
+                <div className={styles.door}>
+                  <div className={styles.door__image}>
+                    <img
+                      className={styles.door__img}
+                      src="images/door-icon.png"
+                      alt="doors"
+                    />
+                  </div>
+                  <div className={styles.doorInfo}>
+                    <div className={styles.logo}>
+                      <img src="images/PLANETA_DOORS.png" alt="logo" />
+                    </div>
+                    <div className={styles.door__info}>
+                      <h3 className={styles.doorName}>?</h3>
+                      <h3 className={styles.height}>Высота: ?</h3>
+                      <p className={styles.info}>?</p>
+                      <h3 className={styles.made}>Сделано: ?</h3>
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.door}>
+                  <div className={styles.door__image}>
+                    <img
+                      className={styles.door__img}
+                      src="images/door-icon.png"
+                      alt="doors"
+                    />
+                  </div>
+                  <div className={styles.doorInfo}>
+                    <div className={styles.logo}>
+                      <img src="images/PLANETA_DOORS.png" alt="logo" />
+                    </div>
+                    <div className={styles.door__info}>
+                      <h3 className={styles.doorName}>?</h3>
+                      <h3 className={styles.height}>Высота: ?</h3>
+                      <p className={styles.info}>?</p>
+                      <h3 className={styles.made}>Сделано: ?</h3>
+                    </div>
+                  </div>
+                </div>
+              </>
+            )}
           </div>
         </div>
       </div>
