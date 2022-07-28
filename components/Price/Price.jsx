840c801990365cf4b838cc642d33e4d4b92e58e2
Azamat <azamatduishenkulov711@gmail.com>
2022-07-28 15:36:04 +0600

    add price

--- components/Price/Price.jsx
+++ components/Price/Price.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import styles from "./Price.module.sass";
 
 export default function Price() {
   return (
-    <div>Price</div>
+    <div className={styles.container}>
+      <div className={styles.block}>
+        <div className={styles.info}>
+          <div className={styles.price__title}>Мы держим низкие цены как оптом так и в розницу</div>
+          <div className={styles.price__des}>Многие из наших конкурентов завышают цены на двери, но из-за того что мы ценим чужой труд, у нас цены на своем уровне</div>
+
+        </div>
+        <img src="images/price-image.png" alt="" className={styles.image}/>
+      </div>
+    </div>
   )
 }
