c520e1ff05611e61622b2d0ea7d6828f26733b97
Erbol <a1darbekov640@gmail.com>
2022-07-28 15:06:23 +0300

    Add location

---
+++ components/Location/Location.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./Location.module.sass";
+
+function Location({ width }) {
+  return (
+    <div className={styles.location}>
+      <div className={styles.location__info}>
+        <h3 className={styles.info__title}>Местоположение</h3>
+        <p className={styles.info__description}>
+          Наши местоположения расположены в очень удобных местах доступны всеми
+          транспортами{" "}
+        </p>
+      </div>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2920.941653064124!2d74.62148491473971!3d42.937356906441806!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x389eb9e2b99cc43f%3A0x13b0c42285a36994!2z0JHQuNC30L3QtdGBLdCm0LXQvdGC0YAgItCQ0KHQnNCQ0J0i!5e0!3m2!1sru!2skg!4v1659006057809!5m2!1sru!2skg"
+        width={width}
+        height="400"
+        style={{ border: "0" }}
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+  );
+}
+
+export default Location;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
 import Head from "next/head";
+import { useEffect, useState } from "react";
 import HeadPage from "../components/HeadPage/HeadPage";
+import Location from "../components/Location/Location";
 import Products from "../components/Products/Products";
 import styles from "../styles/Home.module.sass";
 
 export default function Home() {
+  const [screenWidth, setScreenWidth] = useState(null);
+  const handleWidth = () => {
+    const screenWidth = window.screen.width;
+    setScreenWidth(screenWidth);
+  };
+  useEffect(() => {
+    handleWidth();
+    window.addEventListener("resize", handleWidth);
+    return () => window.removeEventListener("resize", handleWidth);
+  }, []);
   return (
     <>
       <Head>
@@ -14,6 +26,7 @@ export default function Home() {
       <script src="//code.jivosite.com/widget/A7IUO6jnwl" async></script>
       <HeadPage />
       <Products />
+      <Location width={screenWidth} />
     </>
   );
 }
