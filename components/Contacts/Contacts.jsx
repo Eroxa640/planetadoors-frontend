9abfba4fa5626c827d92d9b4df2798c33e69902f
Azamat <azamatduishenkulov711@gmail.com>
2022-07-29 15:30:24 +0600

    add contacts

--- components/Contacts/Contacts.jsx
+++ components/Contacts/Contacts.jsx
@@ -3,6 +3,18 @@ import styles from "./Contacts.module.sass";
 
 export default function Contacts() {
   return (
-    <div>Contacts</div>
+    <div className={styles.container}>
+        <h1 className={styles.title}>Контакты</h1>
+      <div className={styles.block}>
+        <img src="images/contact-image.png" alt="" className={styles.image} />
+        <form action="" className={styles.form}>
+          <input type="text"  placeholder='Имя' className={styles.name} />
+          <input type="text" placeholder='Email' className={styles.email} />
+          <input type="text" placeholder='Номер' className={styles.number} />
+          <textarea type="text" placeholder='Сообщение' className={styles.text} />
+          <button className={styles.btn__contacts}>Отправить</button>
+        </form>
+      </div>
+    </div>
   )
 }
